0a6ad121503bbe3cf75db25a7883cc6011a7c405
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2023-02-11 20:03:33 +0400

    Done contact section

--- src/components/Accordion/index.js
+++ src/components/Accordion/index.js
@@ -7,7 +7,7 @@ const Accordion = () => {
       <h5 className="text-center my-8 font-bold sm:text-[1.875rem] text-[1.5rem]">
         Frequently <span className="text-Teal">Asked Questions</span>
       </h5>
-      <div className="mt-12 max-w-[70%] mx-auto">
+      <div className="mt-12 max-w-[700px]  mx-auto">
         {accordions.map((item) => (
           <AccordionItems key={item.id} {...item} />
         ))}
